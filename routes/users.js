fd5c86cd774aa8076b5093c1d0e1f69e6f250de9
Sam Danger Warner <samwarner@duck.com>
2024-10-09 16:35:03 -0400

    Styled frontend with false API content to cut down on needless API calls

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(requestFromFrontend, responseToFrontend, next) {
+  /** QUERYING THE API: MODELS */
+  // docs: https://metmuseum.github.io/#object
+
+  /** mockup query result based on API docs for the purpose of styling frontend */
+  var desiredReturnedObject = {
+    primaryImageSmallTest: "https://images.metmuseum.org/CRDImages/ep/web-large/DT1467.jpg",
+    primaryImageSmallTestTwo: "https://images.metmuseum.org/CRDImages/as/web-large/DP251139.jpg",
+    objectID: 437133,
+    primaryImage: "https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg",
+    primaryImageSmall:"https://images.metmuseum.org/CRDImages/ep/web-large/DT1567.jpg",
+    title: "Wheat Field with Cypresses",
+    artistDisplayName: "Vincent van Gogh",
+    artistAlphaSort: "Gogh, Vincent van",
+    objectDate: "19th century",
+    medium: "Oil on canvas",
+    artistNationality: "French, born Romania",
+    artistGender: '',
+    artistWikidata_URL: "https://www.wikidata.org/wiki/Q694774",
+    objectWikidata_URL: "https://www.wikidata.org/wiki/Q432253",
+    linkResource: "https://www.metmuseum.org/art/collection/search/547802"
+  }
+
+  responseToFrontend.render('index', { title: 'Classical', apiResult: desiredReturnedObject });
 });
 
 module.exports = router;
